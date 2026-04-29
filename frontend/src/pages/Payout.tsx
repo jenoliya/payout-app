8a74ff3d2efabbe978dde4376714263ffa51eb48
Jenoliya <jenoliyasunilkumar@gmail.com>
2026-04-29 20:54:23 +0530

    http crypto issue fixed

--- frontend/src/pages/Payout.tsx
+++ frontend/src/pages/Payout.tsx
@@ -5,7 +5,13 @@ import { api } from "../api";
 import type { PayoutResponse } from "../types";
 
 function generateIdempotencyKey(): string {
-  return crypto.randomUUID();
+  if (typeof crypto !== "undefined" && crypto.randomUUID) {
+    return crypto.randomUUID();
+  }
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
 }
 
 export default function Payout() {
